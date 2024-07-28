b9d0b03ac0cd07cf2509b52cc06284c1a7db0f10
Amaiyo Praises <amaiyo.praises@gmail.com>
2024-07-28 22:29:30 +0100

    Updated the Client Insight section

---
+++ JS/swiper.js
@@ -0,0 +1,36 @@
+// Testimonial;
+var swiper = new Swiper('.swiper-3', {
+  slidesPerView: 1.2,
+  spaceBetween: 30,
+  freeMode: true,
+  //   centeredSlides: true,
+  //   loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //   autoplay: {
+  //     delay: 4000,
+  //     disableOnInteraction: false,
+  //   },
+
+  //   breakpoints: {
+  //     320: {
+  //       slidesPerView: 1,
+  //       spaceBetween: 15,
+  //     },
+  //     480: {
+  //       slidesPerView: 1.5,
+  //       spaceBetween: 20,
+  //     },
+  //     640: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 30,
+  //     },
+  //   },
+});
