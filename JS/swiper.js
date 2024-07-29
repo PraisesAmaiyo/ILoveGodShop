1acc94cf60f9c5200fc53252159fa0dab37c5f7b
Amaiyo Praises <amaiyo.praises@gmail.com>
2024-07-29 17:27:28 +0100

    Completed the slider Client Insight and General adjustment of the UI

--- JS/swiper.js
+++ JS/swiper.js
@@ -1,36 +1,11 @@
-// Testimonial;
 var swiper = new Swiper('.swiper-3', {
-  slidesPerView: 1.2,
-  spaceBetween: 30,
+  slidesPerView: 'auto',
+  spaceBetween: 15,
   freeMode: true,
-  //   centeredSlides: true,
+  centeredSlides: true,
   //   loop: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  //   autoplay: {
-  //     delay: 4000,
-  //     disableOnInteraction: false,
-  //   },
-
-  //   breakpoints: {
-  //     320: {
-  //       slidesPerView: 1,
-  //       spaceBetween: 15,
-  //     },
-  //     480: {
-  //       slidesPerView: 1.5,
-  //       spaceBetween: 20,
-  //     },
-  //     640: {
-  //       slidesPerView: 2,
-  //       spaceBetween: 30,
-  //     },
-  //   },
 });
