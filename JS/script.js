3c611cb2c99e67913ace535df9c20a3662cb2be3
Amaiyo Praises <amaiyo.praises@gmail.com>
2024-07-31 15:24:15 +0100

    Minor fixes and smooth navigation

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,36 @@
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const allLinks = document.querySelectorAll('a:link');
+const headerEl = document.querySelector('.navigation-header');
+
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    const href = link.getAttribute('href');
+    console.log(href);
+
+    // scroll back to top
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+      e.preventDefault();
+    }
+
+    // CLose mobile navigation
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
+  });
+});
+
 // Toggle the active class for sideNavs
 const sideNavs = document.querySelectorAll('.side-nav_item');
 
